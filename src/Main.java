e5d69444c3ddc869e27f27ec4836d41ca2820396
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-04-09 21:50:48 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> names = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        names.add("Ahmet");
+        names.add("Ayşe");
+        names.add("Mehmet");
+        names.add("Zeynep");
+
+        // Method reference ile liste elemanlarını yazdırma
+        names.forEach(System.out::println);
     }
 }
